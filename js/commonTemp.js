b56a6e96f334cff5456f42406e79aa830d641dc2
NeurotoxinVX <d4:77:62:39:12:4b:7e:3e:ae:b3:70:df:e6:bb:83:d4>
2015-09-21 16:23:32 +0800

    js中增加了基本的方法
    增加了简单jq选择器，以及addClass,removeClass

--- js/commonTemp.js
+++ js/commonTemp.js
@@ -1,3 +1,42 @@
-var Responsive = {
-	
+window.onload = function(){
+	//简单模拟jquery选择器
+	var obj = function(str,parEle){ 
+		str = str.split(" ");
+		var par = [];
+		parEle = parEle||document;
+		var retn = [ parEle ] ;
+		for( var i in str ){ if(str[i].length!=0) par.push(str[i]) } //去掉重复空格
+		for( var i in par ){ 
+			if( retn.length==0 ) return false;
+			var _retn = [];
+			for ( var r in retn )
+			{
+				if( par[i][0] =="#" ) _retn.push( document.getElementById( par[i].replace("#","") ) );
+				else if( par[i][0] =="." ){
+					var tag = retn[r].getElementsByTagName('*');
+					for( var j=0; j<tag.length; j++ ){
+						var cln = tag[j].className;
+						if( cln && cln.search(new RegExp("\\b" + par[i].replace(".","") + "\\b"))!=-1 ){ _retn.push( tag[j] ); }
+					}
+				}
+				else { var tag = retn[r].getElementsByTagName( par[i] ); for( var j=0; j<tag.length; j++ ){ _retn.push( tag[j] ) } }
+			}
+			retn =_retn;
+		}
+		
+		return retn.length==0 || retn[0] == parEle ? false:retn;
+	}// obj E
+
+	// class处理
+	var addClass =function(ele, className){
+		 if (!ele || !className || (ele.className && ele.className.search(new RegExp("\\b" + className + "\\b")) != -1)) return;
+		 ele.className += (ele.className ? " " : "") + className;
+	}
+
+	var removeClass = function(ele, className){
+		 if (!ele || !className || (ele.className && ele.className.search(new RegExp("\\b" + className + "\\b")) == -1)) return;
+		 ele.className = ele.className.replace(new RegExp("\\s*\\b" + className + "\\b", "g"), "");
+	}
+
+
 }
